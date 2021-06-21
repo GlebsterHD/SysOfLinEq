68a1c973f7cd654d28e66bfa8329c63b4bf03209
Gleb Sosnin <sosnin.gleb@yandex.ru>
2021-06-21 21:56:15 +0700

    MatrixException class created

---
+++ MatrixExceptions.py
@@ -0,0 +1,7 @@
+class MatrixException(Exception):
+    def __init__(self, *args):
+        super.__init__(*args)
+        self.msg = args[0] if args else None
+
+    def __str__(self):
+        return f"Error: {self.msg}"
