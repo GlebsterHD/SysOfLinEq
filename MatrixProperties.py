013d96dec73b59f9173d838cc85eded3676ccad2
Gleb Sosnin <sosnin.gleb@yandex.ru>
2021-06-21 10:23:06 +0700

    Add is_numeric function

--- MatrixProperties.py
+++ MatrixProperties.py
@@ -9,3 +9,11 @@ def is_square(matrix: Matrix) -> bool:
         if len(matrix[i]) != len(matrix):
             return False
     return True
+
+
+def is_numeric(matrix: Matrix) -> bool:
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if not isinstance(matrix[i][j], int or float):
+                return False
+    return True
