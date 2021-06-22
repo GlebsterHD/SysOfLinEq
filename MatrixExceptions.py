48e933fc5f8871523f7f23f1656b447485fafa72
Gleb Sosnin <sosnin.gleb@yandex.ru>
2021-06-22 12:02:17 +0700

    Added exceptions in MatrixExceptions.py and rewritten MatrixExceptions.py

--- MatrixExceptions.py
+++ MatrixExceptions.py
@@ -5,3 +5,33 @@ class MatrixException(Exception):
 
     def __str__(self):
         return f"Error: {self.msg}"
+
+
+class MatrixIsNotSquare(MatrixException):
+    def __str__(self):
+        return "Error: Matrix is not square"
+
+
+class MatrixIsNotNumeric(MatrixException):
+    def __str__(self):
+        return "Error: Matrix contains not numeric values"
+
+
+class VectorIsNotNumeric(MatrixException):
+    def __str__(self):
+        return "Error: Vector contains not numeric values"
+
+
+class ListIsNotMatrix(MatrixException):
+    def __str__(self):
+        return "Error: List has different length of lines"
+
+
+class MatrixAndVectorNotMultipliable(MatrixException):
+    def __str__(self):
+        return "Error: Matrix and vector have different dimensions"
+
+
+class MatrixDeterminantIsZero(MatrixException):
+    def __str__(self):
+        return "Error: Matrix determinant is zero"
--- MatrixProperties.py
+++ MatrixProperties.py
@@ -1,51 +1,49 @@
 from typing import List
 
+import MatrixExceptions as me
 
 Matrix = List[list]
 
 
-def is_square(matrix: Matrix) -> bool:
+def is_square(matrix: Matrix):
     for i in range(len(matrix)):
         if len(matrix[i]) != len(matrix):
-            return False
-    return True
+            raise me.MatrixIsNotSquare
 
 
-def is_numeric(matrix: Matrix) -> bool:
+def is_numeric(matrix: Matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if not isinstance(matrix[i][j], int or float):
-                return False
-    return True
+                raise me.MatrixIsNotNumeric
 
 
-def is_vec_numeric(vector: list) -> bool:
+def is_vec_numeric(vector: list):
     for i in range(len(vector)):
         if not isinstance(vector[i], int or float):
-            return False
-    return True
+            raise me.VectorIsNotNumeric
 
 
-def is_matrix(matrix: Matrix) -> bool:
+def is_matrix(matrix: Matrix):
     for i in range(1, len(matrix)):
         if len(matrix[i]) != len(matrix[i - 1]):
-            return False
-    return True
+            raise me.ListIsNotMatrix
 
 
-def is_numeric_matrix(matrix: Matrix) -> bool:
-    return is_numeric(matrix) and is_matrix(matrix)
+def is_numeric_matrix(matrix: Matrix):
+    is_numeric(matrix)
+    is_matrix(matrix)
 
 
-def is_det_calculable(matrix: Matrix) -> bool:
-    return is_numeric_matrix(matrix) and is_square(matrix)
+def is_det_calculable(matrix: Matrix):
+    is_numeric_matrix(matrix)
+    is_square(matrix)
 
 
-def are_multipliable(matrix: Matrix, vec: list) -> bool:
-    if not is_numeric_matrix(matrix) or not is_vec_numeric(vec):
-        return False
+def are_multipliable(matrix: Matrix, vec: list):
+    is_numeric_matrix(matrix)
+    is_vec_numeric(vec)
 
     for i in range(len(matrix)):
         if len(matrix[i]) != len(vec) or not is_numeric_matrix(matrix):
-            return False
-    return True
+            raise me.MatrixAndVectorNotMultipliable
